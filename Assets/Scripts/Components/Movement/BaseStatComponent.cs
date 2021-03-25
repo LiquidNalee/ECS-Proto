d5320e6e2c4b920c79ff7a5ef7b11529a1ae02d9
Lenalee 'LiquidNalee' BOUCENNA <lenalee.boucenna@epita.fr>
2021-03-26 00:01:50 +0100

    Initial Commit

---
+++ Assets/Scripts/Components/Movement/BaseStatComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Components.Movement
+{
+    [Serializable]
+    public struct BaseStatComponent: IComponentData
+    {
+        public float BaseValue;
+        public float Value;
+    }
+}
